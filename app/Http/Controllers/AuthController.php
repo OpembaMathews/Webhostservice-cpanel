a90ae389ed2d4be94edd731c90c56c91f42b9aba
Seun Oyebode <seunoyebode@gmail.com>
2019-09-27 17:42:27 +0100

    Delete User Enabled

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -217,15 +217,17 @@ class AuthController extends Controller
         }
     }
 
-    public function registerAdmin($request)
+    public function deleteUser($id)
     {
-        
+        $user = User::find($id);
+        if($user->type == 'default')
+        {
+            $user->delete();
+            return back()->with('status', 'User Deleted Successfully');
+        }
+        return back()->with('error', 'Cannot Delete Admin User');
     }
 
-    public function registerDefault($request)
-    {
-        
-    }
 
     public function randomString()
     {
